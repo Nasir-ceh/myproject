5f1718970c38466b2330139493abe0f65c1dfa1b
Mohd.Nashirudden <Mohd.Nashirudden@MohdNashir.igglobal.com>
2019-07-31 14:19:15 +0530

    FirstCommit

---
+++ E-connect/src/main/java/com/nasir/econnect/EConnectApplication.java
@@ -0,0 +1,13 @@
+package com.nasir.econnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EConnectApplication.class, args);
+	}
+
+}
